92d492654407803dc89cec1100e7d4797984a874
Renanmcg <101998261+Renanmcg@users.noreply.github.com>
2023-10-28 18:10:42 -0300

    Primeiro commit do projeto Spring Initializr

---
+++ src/test/kotlin/com/authapi/AuthAPI/AuthenticationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.authapi.AuthAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
